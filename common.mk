23661bf5a912fe7470a1f0de3fc4af7f72edff2e
Mrick343 <mrinmoy8429de@gmail.com>
2023-05-04 16:13:46 +0530

    sm8350-common: Adapt to Banana

--- common.mk
+++ common.mk
@@ -315,7 +315,7 @@ PRODUCT_BOOT_JARS += \
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-banana
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
