AOSP-for-venus/platform_device_xiaomi_sm8350-common

sm8350-common: Migrate sensor HAL to AIDL interface
Bug: 247478991
Test: tested on-device (P23) ab/P46941077
Test: `sensor_test -ahal list` and `sensor_test list`
Test: `sensor_test -ahal sample -s0` and `sensor_test sample -s0`

ralph950412: Implementation for Xiaomi devices
Test: m, boot, and test sensors functionality

Change-Id: I40f6ab6b7780c53cd638a1f77c95f6c324dc2d15
Signed-off-by: ralph950412 <ralph950412@gmail.com>

common.mk
@@ -478,7 +478,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@2.1-service.xiaomi-multihal \
+    android.hardware.sensors-service.xiaomi-multihal \
     sensors.xiaomi_sm8350 \
     vendor.qti.hardware.display.mapper@1.1.vendor
 

Mrick343/android_device_oneplus_sm8350-common

Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

common.mk
@@ -348,7 +348,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.oplus-multihal \
+    android.hardware.sensors-service.oplus-multihal \
     libsensorndkbridge \
     sensors.oplus
 